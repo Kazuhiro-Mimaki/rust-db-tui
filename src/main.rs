27d3d8c6d39f621b24219369e73d28a0f6c166c9
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-04-20 23:36:06 +0900

    Add: draw layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders},
+    Terminal,
+};
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode().unwrap();
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture).unwrap();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
+            .split(size);
+        let block = Block::default().title("Block 1").borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+
+        let block_1 = Block::default().title("Block 2-1").borders(Borders::ALL);
+        let block_2 = Block::default().title("Block 2-2").borders(Borders::ALL);
+        let chunks_2 = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(15), Constraint::Percentage(85)].as_ref())
+            .split(chunks[1]);
+        f.render_widget(block_1, chunks_2[0]);
+        f.render_widget(block_2, chunks_2[1]);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode().unwrap();
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )
+    .unwrap();
+    terminal.show_cursor()?;
+
+    Ok(())
 }
