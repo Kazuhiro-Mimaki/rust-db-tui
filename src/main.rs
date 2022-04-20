6c7caf08798a4a666b7b1519e33fe1235aefe4f0
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-04-21 08:57:30 +0900

    Improve: readablity of source code

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use crossterm::{
 };
 use std::{io, thread, time::Duration};
 use tui::{
-    backend::CrosstermBackend,
+    backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout},
     widgets::{Block, Borders},
-    Terminal,
+    Frame, Terminal,
 };
 
 fn main() -> Result<(), io::Error> {
@@ -19,25 +19,7 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
-            .split(size);
-        let block = Block::default().title("Block 1").borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-
-        let block_1 = Block::default().title("Block 2-1").borders(Borders::ALL);
-        let block_2 = Block::default().title("Block 2-2").borders(Borders::ALL);
-        let chunks_2 = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints([Constraint::Percentage(15), Constraint::Percentage(85)].as_ref())
-            .split(chunks[1]);
-        f.render_widget(block_1, chunks_2[0]);
-        f.render_widget(block_2, chunks_2[1]);
-    })?;
+    terminal.draw(|f| render_layout(f))?;
 
     thread::sleep(Duration::from_millis(5000));
 
@@ -52,4 +34,27 @@ fn main() -> Result<(), io::Error> {
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+fn render_layout<B: Backend>(f: &mut Frame<B>) {
+    let size = f.size();
+
+    let block_1 = Block::default().title("Block 1").borders(Borders::ALL);
+    let block_2_1 = Block::default().title("Block 2-1").borders(Borders::ALL);
+    let block_2_2 = Block::default().title("Block 2-2").borders(Borders::ALL);
+
+    let chunks_1 = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
+        .split(size);
+
+    let chunks_2 = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(15), Constraint::Percentage(85)].as_ref())
+        .split(chunks_1[1]);
+
+    f.render_widget(block_1, chunks_1[0]);
+    f.render_widget(block_2_1, chunks_2[0]);
+    f.render_widget(block_2_2, chunks_2[1]);
 }
